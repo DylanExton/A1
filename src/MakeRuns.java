e38744bbf0211edeb4a8db1e211b77dde7fb50be
Dylan_Exton <dylexton@gmail.com>
2018-03-19 18:56:46 +1300

    Commented Code so Far

--- src/MakeRuns.java
+++ src/MakeRuns.java
@@ -5,17 +5,22 @@ import java.io.*;
 
 public class MakeRuns {
 
+    //To check if the integer that the user has input is actually an integer
     public static boolean isStringInteger(String number){
+        //try to parse the integer
         try{
             Integer.parseInt(number);
-        }catch(Exception e ){
+        }
+        //If it catches, is not an integer
+        catch(Exception e ){
             return false;
         }
+        //else return true
         return true;
     }
 
     public static void main(String[] args) {
-        //Check if there are arguments to be resolved
+        //Check if there are arguments in the right order and type
         if(args.length != 2 || isStringInteger(args[0])){
             System.out.println("Usage: MakeRuns <int heapSize, filename file.txt>");
             return;
