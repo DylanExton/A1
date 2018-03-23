0b163a8e5b1afcea317db33c37a82fb6f86a5b77
Dylan_Exton <dylexton@gmail.com>
2018-03-24 12:50:01 +1300

    Got the Heap sort working and beginning on making the output and input methods

--- src/MakeRuns.java
+++ src/MakeRuns.java
@@ -9,36 +9,6 @@ import java.io.IOException;
 
 public class MakeRuns {
 
-    //To check if the integer that the user has input is actually an integer
-    public static boolean isStringInteger(String number){
-        //try to parse the integer
-        try{
-            Integer.parseInt(number);
-        }
-        //If it catches, is not an integer
-        catch(Exception e ){
-            return false;
-        }
-        //else return true
-        return true;
-    }
-
-    //Outputs the root of the heap to the file
-    public static void outputRoot(String[] array, BufferedWriter bw){
-        try {
-            String toGo = array[0];
-            bw.write(toGo);
-            bw.newLine();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void heapSort(String[] array, int useSize){
-        
-    }
-
     public static void main(String[] args) {
         //Check if there are arguments in the right order and type
         if(args.length != 2 || !isStringInteger(args[0]) || Integer.parseInt(args[0]) < 1){
@@ -79,9 +49,7 @@ public class MakeRuns {
                 }
             //Setup the file for output
             BufferedWriter output = new BufferedWriter(new FileWriter("Runs.txt"));
-
-
-
+           initialHeapify(heap,useableArray);
 
 
         }
@@ -89,4 +57,87 @@ public class MakeRuns {
             System.err.println(e.getMessage());
         }
     }
+
+    //To check if the integer that the user has input is actually an integer
+    public static boolean isStringInteger(String number){
+        //try to parse the integer
+        try{
+            Integer.parseInt(number);
+        }
+        //If it catches, is not an integer
+        catch(Exception e ){
+            return false;
+        }
+        //else return true
+        return true;
+    }
+
+    //Outputs the root of the heap to the file
+    public static void outputRoot(String[] array, BufferedWriter bw){
+        try {
+            String toGo = array[0];
+            bw.write(toGo);
+            bw.newLine();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Will heapify a subtree to keep it in heap order. Will recursively heapify the lower subtrees if a change is made
+    public static void heapify(String[] array, int useSize, int index){
+
+        //Setup the indexes for the specified subtree
+        int smallest = index;
+        int left = 2*index + 1;
+        int right = 2*index +2;
+
+        //Checks if the left child is in the range, and if it is smaller than the root of the subtree
+        if(left < useSize && (array[left].compareTo(array[smallest]) < 0)){
+            //If it is, swap it to be the smallest
+            smallest = left;
+        }
+
+        //Checks if the right child is in the range and if it is smaller than the smallest value found so far (root||left)
+        if( right < useSize && (array[right].compareTo(array[smallest]) < 0)){
+            //If it is, swap it as the smallest
+            smallest = right;
+        }
+
+        //If the smallest is not the root
+        if(smallest != index){
+            //Move the root into another reference
+            String swap = array[index];
+            //swap the root with the smallest
+            array[index] = array[smallest];
+            //make the root a child of the smallest
+            array[smallest] = swap;
+
+            //run heapify on the subtree of the swapped value
+            heapify(array, useSize, smallest);
+        }
+
+    }
+
+    //For the initial heapify, needs to run on all of the indicies as it assumes the heap is already in heap order
+    public static void initialHeapify(String[] arr, int maxSize){
+        for(int i = maxSize/2 -1; i >= 0; i--){
+                heapify(arr, maxSize, i);
+        }
+    }
+
+    //Reduces the size of the heap by 1
+    public static int cutHeap(String[] arr, int endIndex){
+        String top = arr[endIndex];
+        arr[endIndex] = arr[0];
+        arr[0] = top;
+
+        return endIndex;
+    }
+
+    //Replaces the root of the heap once it has been output to the file
+    public static void replaceRoot(String[] arr, String toReplace){
+        arr[0] = toReplace;
+    }
+
 }
